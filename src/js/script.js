5eed3ef3a29d15d9eb05b4bf5c1b6aa8cfa94da2
frephs <ffrancescogenovese@gmail.com>
2023-03-19 18:11:41 +0100

    minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -75,7 +75,7 @@ let contacts = [
     new Contact("Mail", "mailto:francescogenovese@duck.com", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvectorified.com%2Fimages%2Femail-icon-white-png-38.png&f=1&nofb=1&ipt=dd837ef3a6fdb55d7f303a848006b5434e0a337ed6ea1b600b1233190334e1f5&ipo=images"),
     new Contact("Github", "https://github.com/frephs", "src/img/github.svg"),
     new Contact("Telegram", "https://frephs.t.me", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpnggrid.com%2Fwp-content%2Fuploads%2F2021%2F04%2FTelegram-Logo-PNG-Transparent-Background-1536x1272.png&f=1&nofb=1&ipt=aebfb2a8db9f8843a5784b6d26c0a13f56150dc0b4adad7a01be4478f93163e8&ipo=images"),
-    new Contact("Instagram", "https://instragram.com/frephs", "https://www.kortegaard.co.uk/wp-content/uploads/2020/06/best-solutions-of-instagram-png-transparent-png-images-unique-white-instagram-logo-outline-of-white-instagram-logo-outline-copy.png"),
+    new Contact("Instagram", "https://instagram.com/frephs", "https://www.kortegaard.co.uk/wp-content/uploads/2020/06/best-solutions-of-instagram-png-transparent-png-images-unique-white-instagram-logo-outline-of-white-instagram-logo-outline-copy.png"),
     new Contact("PayPal", "https://paypal.me/frncscgnvs", "src/img/paypal.png"),
 
 ]
