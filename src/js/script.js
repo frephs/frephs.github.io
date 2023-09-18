f0eb2b213996acde38510463eeb748fc7159488d
frephs <ffrancescogenovese@gmail.com>
2023-09-18 17:52:27 +0200

    add inspiration quotes

--- src/js/script.js
+++ src/js/script.js
@@ -23,7 +23,7 @@ class Project {
     displayPreview() {
         let container = document.querySelectorAll("#previews-container")[0];
         let html =
-        `
+            `
         <a href="${this.link}" target="_blank">
             <div class="project-preview">
                 <div class="iframe-container">
@@ -39,7 +39,7 @@ class Project {
             </div>
         </a>
         `
-    container.innerHTML += html;
+        container.innerHTML += html;
     }
 }
 
@@ -65,6 +65,30 @@ class Contact {
     }
 }
 
+class Quote {
+    constructor(author, source, quote) {
+        this.author = author;
+        this.source = source
+        this.quote = quote;
+    }
+
+    display() {
+        let container = document.querySelectorAll('#quotes-container')[0];
+        let html =
+            `
+        <div class="quote neumorphic">
+            <div class="quote-content">
+                <span class="quote-quote">${this.quote}</span>
+                <hr size= 1>
+                <span class="quote-source">${(this.source) ? this.source : ""}</span>
+                <span class="quote-author">${this.author}</span>
+            </div>
+        </div>
+        `
+        container.innerHTML += html;
+    }
+}
+
 let projects = [
     new Project("GetAulePolimi", "https://get-aule-polimi.vercel.app/", "🏫", true),
     new Project("WeirdlyWired", "https://weirdly-wired.vercel.app/", "🔠", true),
@@ -76,19 +100,26 @@ let projects = [
 let contacts = [
     new Contact("Mail", "mailto:francescogenovese@duck.com", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvectorified.com%2Fimages%2Femail-icon-white-png-38.png&f=1&nofb=1&ipt=dd837ef3a6fdb55d7f303a848006b5434e0a337ed6ea1b600b1233190334e1f5&ipo=images"),
     new Contact("Github", "https://github.com/frephs", "src/img/github.svg"),
-    new Contact("Telegram", "https://frephs.t.me", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpnggrid.com%2Fwp-content%2Fuploads%2F2021%2F04%2FTelegram-Logo-PNG-Transparent-Background-1536x1272.png&f=1&nofb=1&ipt=aebfb2a8db9f8843a5784b6d26c0a13f56150dc0b4adad7a01be4478f93163e8&ipo=images"),
+    new Contact("Telegram", "https://frephs.t.me", "src/img/telegram.svg"),
     new Contact("Instagram", "https://instagram.com/frephs", "https://www.kortegaard.co.uk/wp-content/uploads/2020/06/best-solutions-of-instagram-png-transparent-png-images-unique-white-instagram-logo-outline-of-white-instagram-logo-outline-copy.png"),
     new Contact("PayPal", "https://paypal.me/frncscgnvs", "src/img/paypal.png"),
     new Contact("GoodReads", "https://goodreads.com/frephs/", "src/img/goodreads.svg"),
 ]
 
+let quotes = [
+    new Quote("Lucretius", "De rerum natura", "Rerum magnarum parva potest res <br>exemplare dare et vestigia notitiai. <br> <br>A small thing can give an example of great things <br> and impart knowledge of their traces. "),
+    new Quote("Edsger W. Dijkstra", undefined, "The purpose of abstraction is not to be vague, but to create a new semantic level in which one can be absolutely precise."),
+    new Quote("Karl Marx", undefined, "In modern industry the means of production must become means of production in common, social means of production, and, as such, they are employed in common, and produce, not privately for the individual producer, but socially.  <br> <br> From each according to their ability, to everyone according to their needs. <br> <br>  The ideas of the ruling class are in every epoch the ruling ideas, i.e., the class which is the ruling material force of society, is at the same time its ruling intellectual force. ")
+]
+
+quotes.forEach(quote => quote.display());
+
 projects.forEach(
     project => {
         project.display()
-        if(project.preview == true)
+        if (project.preview == true)
             project.displayPreview()
     }
 );
 
 contacts.forEach(contact => contact.display());
-
