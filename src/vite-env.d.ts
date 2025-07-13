bba38139badb72ed4abe025da02de33a9c18226f
frephs <ffrancescogenovese@gmail.com>
2025-07-13 11:00:22 +0200

    fix: build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,51 @@
-/// <reference types="vite/client" />
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.less' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
