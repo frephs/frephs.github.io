92171338c50d9d90a79e420a385ee59d4e2a322a
frephs <ffrancescogenovese@gmail.com>
2025-07-13 11:51:05 +0200

    refactor: change variable declarations from let to const for better clarity

--- src/components/WaveAnimation.tsx
+++ src/components/WaveAnimation.tsx
@@ -17,19 +17,19 @@ export const WaveAnimation: React.FC<WaveAnimationProps> = ({ className, style }
 
       let paths = "";
       
-      let minY = -100 * Math.tan(Math.PI / 6) - 30;
+      const minY = -100 * Math.tan(Math.PI / 6) - 30;
       
       let delta = 1;
       let deltaDelta = 0.25;
       
       for (let y = minY; y < 100; y += (delta += (deltaDelta += 0.01))) {
-        let pA = [100, y];
-        let pB = [0, y + 100 * Math.tan(Math.PI / 6)];
-        let rA = 100 / 1.5;
-        let thetaA = Math.PI * (-0.15 + 0.3 + 0.3 * Math.sin(y / 150 + 0.6 + n / 100));
-        let rB = 100 / 2;
-        let thetaB = Math.PI * (-0.2 + 0.3 + 0.3 * Math.sin(y / 150 + 1.3 + n / 110));
-        let d = "M" + pA[0] + "," + pA[1] +
+        const pA = [100, y];
+        const pB = [0, y + 100 * Math.tan(Math.PI / 6)];
+        const rA = 100 / 1.5;
+        const thetaA = Math.PI * (-0.15 + 0.3 + 0.3 * Math.sin(y / 150 + 0.6 + n / 100));
+        const rB = 100 / 2;
+        const thetaB = Math.PI * (-0.2 + 0.3 + 0.3 * Math.sin(y / 150 + 1.3 + n / 110));
+        const d = "M" + pA[0] + "," + pA[1] +
                 "C" +
                 (pA[0] - rA * Math.cos(thetaA)) + "," +
                 (pA[1] + rA * Math.sin(thetaA)) + "," +
@@ -40,19 +40,19 @@ export const WaveAnimation: React.FC<WaveAnimationProps> = ({ className, style }
         paths += "<path d='" + d + "' class='wave-path'></path>";
       }
       
-      let maxX = 100 + 100 * Math.tan(Math.PI / 6);
+      const maxX = 100 + 100 * Math.tan(Math.PI / 6);
       
       delta = 1;
       deltaDelta = 0.3;
       
       for (let x = 0; x < maxX; x += (delta += (deltaDelta += 0.01))) {
-        let pA = [x, 0];
-        let pB = [x - 100 * Math.tan(Math.PI / 6), 100];
-        let rA = 100 / 3;
-        let thetaA = Math.PI * (0.6 + 0.15 * Math.sin(x / 150 + 0.3 + n / 120));
-        let rB = 100 / 3;
-        let thetaB = Math.PI * (0.5 + 0.15 * Math.sin(x / 150 + 1.9 + n / 200));
-        let d = "M" + (100 - pA[0]) + "," + pA[1] +
+        const pA = [x, 0];
+        const pB = [x - 100 * Math.tan(Math.PI / 6), 100];
+        const rA = 100 / 3;
+        const thetaA = Math.PI * (0.6 + 0.15 * Math.sin(x / 150 + 0.3 + n / 120));
+        const rB = 100 / 3;
+        const thetaB = Math.PI * (0.5 + 0.15 * Math.sin(x / 150 + 1.9 + n / 200));
+        const d = "M" + (100 - pA[0]) + "," + pA[1] +
                 "C" +
                 ((100 - pA[0]) - rA * Math.cos(thetaA)) + "," +
                 (pA[1] + rA * Math.sin(thetaA)) + "," +
